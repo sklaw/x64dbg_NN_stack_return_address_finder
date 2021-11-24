87a33d0bd17b8fd9128f8152661335ad5d3aac49
S.K. Law <sk.law.lsq@gmail.com>
2021-11-23 23:53:43 -0500

    Got debugee csp and stack base.

--- x64dbg_script_dll_retaddrfinder_project/x64dbg_script_dll_retaddrfinder_project.cpp
+++ x64dbg_script_dll_retaddrfinder_project/x64dbg_script_dll_retaddrfinder_project.cpp
@@ -6,17 +6,27 @@
 #include "x64dbg_script_dll_retaddrfinder_project.h"
 #include "pluginsdk/bridgemain.h"
 #include "pluginsdk/TitanEngine/TitanEngine.h"
+#include <winternl.h>
+#include <winnt.h>
 
 extern "C" X64DBGSCRIPTDLLRETADDRFINDERPROJECT_API void Start() {
 	duint addr = 0x1234;
 	STACK_COMMENT comment;
 	DbgStackCommentGet(addr, &comment);
 
-	HANDLE hActiveThread = DbgGetThreadHandle();
-	duint csp = GetContextDataEx(hActiveThread, UE_CSP);
+	HANDLE debugee_hActiveThread = DbgGetThreadHandle();
+	duint debugee_csp = GetContextDataEx(debugee_hActiveThread, UE_CSP);
 
-	duint size;
-	duint base = DbgMemFindBaseAddr(csp, &size);
+	DWORD debugee_active_thread_tid = DbgGetThreadId();
 
-	DWORD tid = DbgGetThreadId();
+	duint debugee_active_thread_teb_vaddr = DbgGetTebAddress(debugee_active_thread_tid);
+	duint debugee_active_thread_tib_vaddr = debugee_active_thread_teb_vaddr;
+
+	NT_TIB debugee_active_thread_tib;
+
+	bool succeed = DbgMemRead(debugee_active_thread_tib_vaddr, &debugee_active_thread_tib, sizeof(NT_TIB));
+
+	PVOID debugee_stack_base = debugee_active_thread_tib.StackBase;
+
+
 }
