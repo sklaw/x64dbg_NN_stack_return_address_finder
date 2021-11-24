1b06949da67895127a31e5e9963172de377b7e41
S.K. Law <sk.law.lsq@gmail.com>
2021-11-24 16:16:24 -0500

    Print stack dump in debugger log

--- x64dbg_script_dll_retaddrfinder_project/x64dbg_script_dll_retaddrfinder_project.cpp
+++ x64dbg_script_dll_retaddrfinder_project/x64dbg_script_dll_retaddrfinder_project.cpp
@@ -8,6 +8,7 @@
 #include "pluginsdk/TitanEngine/TitanEngine.h"
 #include <winternl.h>
 #include <winnt.h>
+#include <cstring>
 
 extern "C" X64DBGSCRIPTDLLRETADDRFINDERPROJECT_API void Start() {
 	duint addr = 0x1234;
@@ -25,8 +26,37 @@ extern "C" X64DBGSCRIPTDLLRETADDRFINDERPROJECT_API void Start() {
 	NT_TIB debugee_active_thread_tib;
 
 	bool succeed = DbgMemRead(debugee_active_thread_tib_vaddr, &debugee_active_thread_tib, sizeof(NT_TIB));
+	if (!succeed) {
+		GuiAddLogMessage("Fail to read debugee TIB.\n");
+		return;
+	}
 
-	PVOID debugee_stack_base = debugee_active_thread_tib.StackBase;
 
 
+	duint debugee_stack_base = (duint)debugee_active_thread_tib.StackBase;
+
+	int dump_size = debugee_stack_base - debugee_csp;
+	char* stack_dump = new char[dump_size];
+
+	succeed = DbgMemRead(debugee_csp, stack_dump, dump_size);
+	if (!succeed) {
+		GuiAddLogMessage("Fail to read debugee stack dump.\n");
+		return;
+	}
+
+	void* tmp;
+	int ptr_size = sizeof(tmp);
+	char msg[100+ MAX_COMMENT_SIZE];
+	STACK_COMMENT sc;
+	duint stack_addr;
+
+	for (int i = dump_size % ptr_size; i < dump_size; i += ptr_size) {
+		stack_addr = debugee_csp + i;
+		succeed = DbgStackCommentGet(stack_addr, &sc);
+		if (strcmp(sc.color, "!rtnclr") == 0) {
+			tmp = *(void**)(&stack_dump[i]);
+			sprintf_s(msg, "%p\t%s\n", tmp, sc.comment);
+			GuiAddLogMessage(msg);
+		}
+	}
 }
